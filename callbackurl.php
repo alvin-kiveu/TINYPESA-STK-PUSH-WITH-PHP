e0d85dd3db1b7a35376a124b4efdddb34ab971b0
Alvin Kiveu <67068458+alvin-kiveu@users.noreply.github.com>
2021-11-28 00:41:41 -0800

    callback url for geting transacion

---
+++ callbackurl.php
@@ -0,0 +1,34 @@
+<?php
+header("Content-Type: application/json");
+
+$stkCallbackResponse = file_get_contents('php://input');
+$logFile = "stkTinypesaResponse.json";
+$log = fopen($logFile, "a");
+fwrite($log, $stkCallbackResponse);
+fclose($log);
+
+$callbackContent = json_decode($stkCallbackResponse);
+
+$ResultCode = $callbackContent->Body->stkCallback->ResultCode;
+$CheckoutRequestID = $callbackContent->Body->stkCallback->CheckoutRequestID;
+$Amount = $callbackContent->Body->stkCallback->CallbackMetadata->Item[0]->Value;
+$MpesaReceiptNumber = $callbackContent->Body->stkCallback->CallbackMetadata->Item[1]->Value;
+$PhoneNumber = $callbackContent->Body->stkCallback->CallbackMetadata->Item[4]->Value;
+
+if ($ResultCode == 0) {
+    $servername = "localhost";
+    $username = "umeskias_umeskia_user";
+    $password = "1qbejxBr%blT";
+    $dbname = "umeskias_umskiasoftwares";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $insert = $conn->query("INSERT INTO tinypesa(CheckoutRequestID,ResultCode,amount,MpesaReceiptNumber,PhoneNumber) VALUES ('$CheckoutRequestID','$ResultCode','$Amount','$MpesaReceiptNumber','$PhoneNumber')");
+
+    $conn = null;
+}
